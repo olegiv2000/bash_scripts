c19aec968bc24dbdede10dbb4ea52342bc74aa1c
olegiv2000 <81378542+olegiv2000@users.noreply.github.com>
2021-03-28 20:05:40 +0300

    Update os_info_script.sh

--- os_info_script.sh
+++ os_info_script.sh
@@ -5,6 +5,7 @@ echo -e "\nUsername:" "\n$USER" >>/tmp/os_info/os_info_output.txt;
 echo -e "\nHostname:" "\n$HOSTNAME" >>/tmp/os_info/os_info_output.txt;
 echo -e "\nInterface config:" "\n$(ifconfig -s)" >>/tmp/os_info/os_info_output.txt;
 echo -e "\nNetwork interfaces:" "\n$(ip link show)" >>/tmp/os_info/os_info_output.txt;
+echo -e "\nFirewall status:" "\n$(sudo ufw status)" >>/tmp/os_info/os_info_output.txt;
 echo -e "\nFirewall version:" "\n$(sudo ufw version)" >>/tmp/os_info/os_info_output.txt;
 echo -e "\nServername:" "\n$(cat /etc/resolv.conf| grep nameserver)" >>/tmp/os_info/os_info_output.txt;
 echo -e "\nThe number of seconds the script has been running for:" "\n$SECONDS" >>/tmp/os_info/os_info_output.txt
